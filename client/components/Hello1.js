659c86c1141babe10522cd13f698aed331af86b7
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-03-21 21:43:00 +0100

    Remove div from arrow-functional-component

--- client/components/Hello1.js
+++ client/components/Hello1.js
@@ -1,10 +1,9 @@
 import React, { PropTypes } from 'react';
 
-const Hello1 = (props, context) => (
-  <div>
-  	<h1>Test arrow function</h1>
-  </div>
-);
+function Hello1(props, context) {
+  return (
+  );
+}
 
 Hello1.propTypes = {
 };
--- client/modules/Post/pages/PostDetailPage/PostDetailPage.js
+++ client/modules/Post/pages/PostDetailPage/PostDetailPage.js
@@ -12,18 +12,47 @@ import { fetchPost } from '../../PostActions';
 // Import Selectors
 import { getPost } from '../../PostReducer';
 
-export function PostDetailPage(props) {
-  return (
-    <div>
-      <Helmet title={props.post.title} />
-      <div className={`${styles['single-post']} ${styles['post-detail']}`}>
-        <h3 className={styles['post-title']}>{props.post.title}</h3>
-        <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
-        <p className={styles['post-desc']}>{props.post.content}</p>
+export class PostDetailPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {props} = this;
+    return(
+      <div>
+        <Helmet title={props.post.title} />
+        <div className={`${styles['single-post']} ${styles['post-detail']}`}>
+          <h3 className={styles['post-title']}>{props.post.title}</h3>
+          <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
+          <p className={styles['post-desc']}>{props.post.content}</p>
+        </div>
       </div>
+    );
+  }
+}
+
+renderPostForm = () => {
+  return (
+    <div className={styles['form-content']}>
+      <h2 className={styles['form-title']}><FormattedMessage id="editPost" /></h2>
+      <input placeholder={this.props.intl.messages.authorName} className={styles['form-field']} name="name" value={this.state.name} onChange={this.handleInputChange}/>
+      <input placeholder={this.props.intl.messages.postTitle} className={styles['form-field']} name="title" value={this.state.title} onChange={this.handleInputChange}/>
+      <textarea placeholder={this.props.intl.messages.postContent} className={styles['form-field']} name="content" value={this.state.content} onChange={this.handleInputChange}/>
+      <a className={styles['post-submit-button']} href="#" onClick={this.handleEditPost}><FormattedMessage id="submit" /></a>
     </div>
   );
-}
+};
+
+renderPost = () => {
+  return (
+    <div className={`${styles['single-post']} ${styles['post-detail']}`}>
+      <h3 className={styles['post-title']}>{this.props.post.title}</h3>
+      <p className={styles['author-name']}><FormattedMessage id="by" /> {this.props.post.name}</p>
+      <p className={styles['post-desc']}>{this.props.post.content}</p>
+    </div>
+  );
+};
 
 // Actions required to provide data for this component to render in server side.
 PostDetailPage.need = [params => {
