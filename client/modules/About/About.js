37961e871dbd06198b5c2bff24345ec3866122ce
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-03-27 22:15:39 +0200

    Added modules home and about

---
+++ client/modules/About/About.js
@@ -0,0 +1,36 @@
+import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+// Import Style
+// import styles from "./About.css";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+About.propTypes = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
